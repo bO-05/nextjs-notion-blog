6589ea254bda3383531160ed69e1b03eb24a0498
bO-05 <blessedbyhope05@gmail.com>
2026-05-06 22:30:01 +0700

    fix: map notion custom emoji icons locally

--- lib/map-image-url.ts
+++ lib/map-image-url.ts
@@ -3,7 +3,13 @@ import { defaultMapImageUrl } from 'notion-utils'
 
 import { defaultPageCover, defaultPageIcon } from './config'
 
+const localPageIconUrl = '/favicon-192x192.png?v=20260506'
+
 export const mapImageUrl = (url: string, block: Block) => {
+  if (url?.startsWith('notion://custom_emoji/')) {
+    return localPageIconUrl
+  }
+
   if (url === defaultPageCover || url === defaultPageIcon) {
     return url
   }
